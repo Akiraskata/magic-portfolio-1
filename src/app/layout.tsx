21c212f23fc15c5302d3b29daeac7b0ec47fd354
Akira Kata <akirakata@icloud.com>
2026-02-20 22:49:23 +0900

    tawk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,24 @@ export default async function RootLayout({
   `}
           </Script>
 
+          {/* Tawk.to 聊天窗口 */}
+          <Script
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                  __html: `
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/69985e3e9d60291c30385b7e/1jhtj09qf';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+        })();
+      `,
+              }}
+          />
+
 
         <script
           id="theme-init"
@@ -122,6 +140,9 @@ export default async function RootLayout({
             `,
           }}
         />
+
+
+
       </head>
       <Providers>
         <Column
